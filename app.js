204499cef0c095559f2badb571c33c94fbcb5f43
andrumahardi <andrumahardi77@gmail.com>
2020-07-05 02:09:54 +0700

    done

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-const cors = require('cors')
-const express = require('express')
-const app = express()
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
- // Add this
- if (req.method === 'OPTIONS') {
-
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
-      res.header('Access-Control-Max-Age', 120);
-      return res.status(200).json({});
-  }
-
-  next()
-})
-
+const app = require('express')()
 const server = app.listen(3000)
-
-const io = require('socket.io')(server)
+const io = require('socket.io').listen(server)
 const fs = require('fs')
 
 
