7ea020e0d24ad21a2e726bdc264b6782b2c5e490
andrumahardi <andrumahardi77@gmail.com>
2020-07-05 01:12:49 +0700

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const cors = require('cors')
 const app = require('express')()
 const server = app.listen(3000)
 const io = require('socket.io').listen(server)
 const fs = require('fs')
+
+app.use(cors())
 
 let messages = []
 let onlineClients = []
