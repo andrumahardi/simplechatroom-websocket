aea5ebf6301510094bd9434bcafec624895fb735
andrumahardi <andrumahardi77@gmail.com>
2020-07-05 02:19:47 +0700

    done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const app = require('express')()
+const cors = require('cors')
 
+
+app.use(cors())
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
